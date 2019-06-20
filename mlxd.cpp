1add1773dd98d85321f2f6b5cab097c730996c29
Dave.C <g8xqp@users.noreply.github.com>
2019-06-20 16:25:35 +0100

    Add files via upload

--- mlxd.cpp
+++ mlxd.cpp
@@ -9,6 +9,14 @@
 // 50: 50 51 52 53 54 55 56 57 -- -- -- -- -- -- -- -- 
 // 60: 60 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- 
 // 70: -- -- -- -- -- -- -- --                         
+//
+// Makefile
+//  LIBS=-lm -lbcm2835
+//  mlxd: mlxd.cpp
+//     	cc -o mlxd mlxd.cpp $(LIBS)
+//
+// 2019 June 20 --- dmc
+//
 
 #include <fcntl.h>
 #include <stdio.h>
@@ -26,35 +34,58 @@ class command_line{
 private:
   int Ncalls;
   int HelpLevel;
-  int VerbosityLevel;
+  int VerbLevel;
   int TestLevel;
   bool ShowVers;
   bool WriteFIFO;
   int CommandLineError;
+  int CheckExactMatch(int argc, char **argv,int i,char * s){
+    if(i<argc){
+      if(0==strcmp(argv[i],s))return(0);
+    }
+    return(-1);
+  }
+  int CheckMatchWithParam(int argc, char **argv,int i,char * s,char ** x){
+    if(i<(argc+1)){
+	if(0==strcmp(argv[i],s)){
+	  *x=argv[i+1];
+	return(0);
+      }
+    }
+    return(-1);
+  }
+  int CheckMatchWithInt(int argc, char **argv,int i,char * s,int * n){
+    char * endptr;
+    if(i<(argc+1)){
+      if(0==strcmp(argv[i],s)){
+	*n=strtol(argv[i+1],&endptr,10);
+	if((endptr-argv[i+1])==strlen(argv[i+1]))return(0);
+      }
+    }
+    return(-1);
+  }
+public:
   void ShowVersion(char * cmd){
     printf("This program [%s] reads data from Melexis MLX90621\n",cmd);
     printf("Build date %s, %s - dmc\n",__DATE__,__TIME__);
   }
   void ShowHelp(char * cmd){
     printf("This program reads data from Melexis MLX90621\n");
-    printf("chip and calculates temperature readings.  Needs root\n");
-    printf("permission to read from chip and write to fifo.\n");
+    printf("chip and calculates temperature readings.\n");
+    printf("Needs root permission to read from chip and write to fifo.\n");
     printf("Command line options listed below.\n");
     printf("%s -h          Show help\n",cmd);
-    printf("%s -Version    Show version\n",cmd);
-    printf("%s -v0         Verbosity level 0\n",cmd);
-    printf("%s -v1         Verbosity level 1\n",cmd);
-    printf("%s -v2         Verbosity level 2\n",cmd);
+    printf("%s -version    Show version\n",cmd);
+    printf("%s -v n        Verbosity level n (n is integer 1,2,3)\n",cmd);
     printf("%s -wf         Write temperature data to fifo\n",cmd);
-    printf("%s -t0         Test mode 0 (write test array to fifo)\n",cmd);
-    printf("%s -t1         Test mode 1 (read array from fifo)\n",cmd);
+    printf("%s -t 1        Test mode 1 (write test array to fifo)\n",cmd);
+    printf("%s -t 2        Test mode 2 (read array from fifo)\n",cmd);
   }
-public:
   int GetHelpLevel(){
     return(HelpLevel);
   }
   int GetVerbosityLevel(){
-    return(VerbosityLevel);
+    return(VerbLevel);
   }
   int GetTestLevel(){
     return(TestLevel);
@@ -66,41 +97,35 @@ public:
     return(WriteFIFO);
   }
   int ParseCommandLine(int argc, char **argv){
-    int i=0;
+    int i=1;
+    char *x;
+    int n=0;
     if(Ncalls==0){ // Only check once
       HelpLevel=0;
-      VerbosityLevel=0;
+      VerbLevel=0;
       TestLevel=0;
       ShowVers=false;
       WriteFIFO=false;
       CommandLineError=0;
-      do{
-        //      printf("i=%d %d [%s]\n",i,argc,argv[i]);
-        if(i!=0){
-          if (0==strcmp(argv[i],"-help")){
-            HelpLevel=2;
-          } else if (0==strcmp(argv[i],"-h")){
-            HelpLevel=1;
-          } else if (0==strcmp(argv[i],"-Version")) {
-              ShowVers=true;
-          } else if (0==strcmp(argv[i],"-v0")) {
-              VerbosityLevel=1;
-          } else if (0==strcmp(argv[i],"-v1")) {
-            VerbosityLevel=2;
-          } else if (0==strcmp(argv[i],"-v2")) {
-            VerbosityLevel=3;
-          } else if (0==strcmp(argv[i],"-wf")) {
-            WriteFIFO=true;
-          } else if (0==strcmp(argv[i],"-t0")) {
-            TestLevel=1;
-          } else if (0==strcmp(argv[i],"-t1")) {
-            TestLevel=2;
-          } else {
-            CommandLineError++;
-          }
-        }
-        i++;
-      }while(i<argc);
+      if(argc>1){
+	do{
+	  //	printf("i=%d %d [%s]\n",i,argc,argv[i]);
+	  if (0==CheckExactMatch(argc,argv,i,(char*)"-version")){
+	    ShowVers=true;;
+	  } else if (0==CheckExactMatch(argc,argv,i,(char*)"-h")){
+	    HelpLevel=1;
+	  } else if (0==CheckMatchWithInt(argc,argv,i,(char*)"-v",&VerbLevel)) {
+	    i++; // if((VerbLevel<1)|(VerbLevel>3))CommandLineError++;
+	  } else if (0==CheckExactMatch(argc,argv,i,(char*)"-wf")){
+	    WriteFIFO=true;
+	  } else if (0==CheckMatchWithInt(argc,argv,i,(char*)"-t",&TestLevel)) {
+	    i++;
+	  } else {
+	    CommandLineError++;
+	  }
+	  i++;
+	}while(i<argc);
+      }
       if((CommandLineError>0)|ShowVers)ShowVersion(argv[0]);
       if((CommandLineError>0)|(HelpLevel!=0))ShowHelp(argv[0]);
     }
@@ -151,6 +176,24 @@ class fifo_file{
     close(fd);
     return(0);
   }
+  void ShowArray(char * title,float * array, int size){
+    int j;
+    printf("%s :=\n    ",title);
+    for(j=0;j<16;j++)printf("   %02x%c",j,(j==15)?'\n':' ');
+    for(j=0;j<size;j++){
+      if((j%16)==0)printf("%02x: ",j);
+      printf("%05.1f%c",array[j],(((j%16)==15)|(j==(size-1)))?'\n':' ');
+    }
+  }
+  void ShowArray(char * title,int * array, int size){
+    int j;
+    printf("%s :=\n    ",title);
+    for(j=0;j<16;j++)printf("   %02x%c",j,(j==15)?'\n':' ');
+    for(j=0;j<size;j++){
+      if((j%16)==0)printf("%02x: ",j);
+      printf(" %04x%c",array[j],(((j%16)==15)|(j==(size-1)))?'\n':' ');
+    }
+  }
   fifo_file(){ }
   ~fifo_file(){ CloseIO(); }
 }fifo;
@@ -162,10 +205,10 @@ private:
     bcm2835_i2c_setSlaveAddress(SlaveAddress);
     if(ReadSize==0){
       return( ( bcm2835_i2c_write(WriteArray, WriteSize)
-                == BCM2835_I2C_REASON_OK )? 0 : -1 );
+		== BCM2835_I2C_REASON_OK )? 0 : -1 );
     } else {
       return( ( bcm2835_i2c_write_read_rs(WriteArray,WriteSize,ReadArray,ReadSize)
-                == BCM2835_I2C_REASON_OK ) ? 0 : -1 );
+		== BCM2835_I2C_REASON_OK ) ? 0 : -1 );
     }    // return 0 is good
   }
 #define EESIZE 256
@@ -224,7 +267,7 @@ private:
   }
   int write_config(char * Array ) {
     char write_config[] = { 0x03,(char)( Array[0]-0x55), Array[0],
-                            (char)(Array[1]-0x55), Array[1] };
+			    (char)(Array[1]-0x55), Array[1] };
     return(WriteReadI2C(0x60,write_config,5,NULL,0));
   }
   int write_config(int x){
@@ -233,7 +276,7 @@ private:
   }
   int write_trim(char * Array) {
     char write_trim[] = { 0x04, (char)(Array[0]-0xAA), Array[0],
-                          (char)( Array[1]-0xAA), Array[1] };
+			  (char)( Array[1]-0xAA), Array[1] };
     return(WriteReadI2C(0x60,write_trim,5,NULL,0));
   }
   int write_trim(int x){
@@ -306,7 +349,7 @@ private:
       printf("   %02x%c",array[j],(((j%16)==15)|(j==(size-1)))?'\n':' ');
     }
   }
-    
+  
   void ShowArray(char * title,float * array, int size){
     int j;
     printf("%s :=\n    ",title);
@@ -331,13 +374,13 @@ public:
       printf("Vcomp= %7.2f\n",Vcomp);
       printf("Sens = %.3e\n",Sens);
       if(Verbose>=3){
-        char ArrayName[30];
-        strcpy(ArrayName,"Tarray Aij");
-        ShowArray(ArrayName,Aij,64);
-        strcpy(ArrayName,"Tarray Bij");
-        ShowArray(ArrayName,Bij,64);
-        strcpy(ArrayName,"Tarray Cij");
-        ShowArray(ArrayName,Cij,64);
+	char ArrayName[30];
+	strcpy(ArrayName,"Tarray Aij");
+	ShowArray(ArrayName,Aij,64);
+	strcpy(ArrayName,"Tarray Bij");
+	ShowArray(ArrayName,Bij,64);
+	strcpy(ArrayName,"Tarray Cij");
+	ShowArray(ArrayName,Cij,64);
       }
     }
   }
@@ -365,23 +408,30 @@ public:
       ShowArray(ArrayName,TempDeg,64);
     }
   }
+  int CheckBrownOut(){ // 1 is good, otherwise need to initialise again
+    if(read_config()==0){ 
+      return((get_config()&0x400)==0?0:1);
+    } else { 
+      return(-1);
+    }
+  }
   int Init(int Verbose) {
     if (bcm2835_init()){
       bcm2835_i2c_begin();
       bcm2835_i2c_set_baudrate(400000);
       usleep(5000); // 5ms
       if ( read_eeprom()==0 ) {
-        if ( write_trim( eepromArray[0xF7] ) == 0 ) {
-          if ( write_config( eepromArray[0xF5] + ( eepromArray[0xF6]<<8 ) ) == 0){
-            // modify update rate ?
-            if ( read_config() == 0){
-              //printf("Config = %04x\n",get_config());
-              ExtractCoeff();
-              ShowCoeff(Verbose);
-              return(0); // Successful read of eeprom and setting config
-            }
-          }
-        }
+	if ( write_trim( eepromArray[0xF7] ) == 0 ) {
+	  if ( write_config( eepromArray[0xF5] + ( eepromArray[0xF6]<<8 ) ) == 0){
+	    // modify update rate ?
+	    if ( read_config() == 0){
+	      //printf("Config = %04x\n",get_config());
+	      ExtractCoeff();
+	      ShowCoeff(Verbose);
+	      return(0); // Successful read of eeprom and setting config
+	    }
+	  }
+	}
       }
     }
     return(-1);
@@ -398,10 +448,8 @@ void got_sigint(int sig) {
   exit(0);  
 }
 
-
 int main(int argc, char **argv){
-  float  Ftemperature[64];
-  float FTest[64];
+  float FTemperature[64];
   int i,j=0;
   signal(SIGINT, got_sigint);
   
@@ -412,41 +460,45 @@ int main(int argc, char **argv){
     if(cl.GetTestLevel()==1){ // Create float point test array and write to fifo
       fifo.OpenIO();
       do{
-        printf("j=%d\n",j);
-        for(i=0;i<64;i++)FTest[i]=i*2.3;
-        FTest[j]=17;
-        fifo.WriteIO(FTest,64);
-        if(j<63){
-          j++;
-        } else {
-        j=0;
-        }
-        sleep(1);
+	printf("j=%d\n",j);
+	for(i=0;i<64;i++){
+	  FTemperature[i]=i*2.3;
+	}FTemperature[j]=20;
+	fifo.WriteIO(FTemperature,64);
+	if(cl.GetVerbosityLevel()!=0)fifo.ShowArray((char *)"ARRAY",FTemperature,64);
+	if(j<63){
+	  j++;
+	} else {
+	  j=0;
+	}
+	sleep(1);
       } while(1);
+
     } else if(cl.GetTestLevel()==2){ // read and display numbers from fifo
       fifo.OpenIO();
-      for(i=0;i<64;i++)FTest[i]=0;
+      for(i=0;i<64;i++)FTemperature[i]=0;
       do{
-        fifo.ReadIO(FTest,64);
-        for(i=0;i<64;i++)printf("%5.1f %s",FTest[i],((i%16)==15)?"\n":"   ");
+	fifo.ReadIO(FTemperature,64);
+	fifo.ShowArray((char *)"ARRAY",FTemperature,64);
       } while(1);
     } else {      // readand display temperature
       if(cl.GetWriteFIFO())fifo.OpenIO();
       if(melexis.Init(cl.GetVerbosityLevel())==0){
-        do{
-          while(melexis.CheckBrownOut()!=1){
-            sleep(1);
-            printf("Init = %d\n",melexis.Init(cl.GetVerbosityLevel()));
-            if(cl.GetVerbosityLevel()!=0)melexis.ShowEEPROM();
-          }
-          melexis.ShowTarray(cl.GetVerbosityLevel());
-          for(i=0;i<64;i++)Ftemperature[i]=melexis.ReadTemp(i);
-          if(cl.GetWriteFIFO()) fifo.WriteIO(Ftemperature,64);
-          sleep(1);
-        } while(1);
+	do{
+	  while(melexis.CheckBrownOut()!=1){
+	    sleep(1);
+	    printf("Init = %d\n",melexis.Init(cl.GetVerbosityLevel()));
+	    if(cl.GetVerbosityLevel()!=0)melexis.ShowEEPROM();
+	  }
+	  melexis.ShowTarray(cl.GetVerbosityLevel());
+	  for(i=0;i<64;i++)FTemperature[i]=melexis.ReadTemp(i);
+	  if(cl.GetWriteFIFO())	fifo.WriteIO(FTemperature,64);
+	  sleep(1);
+	} while(1);
       }
     }
   }
-}    
+}
+
 
 
